a2754fb0357d9819108a1ba860a1d78f7c230cc5
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-05 14:16:31 -0300

    criando um novo tecnico finalizado

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -1,17 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {API_CONFIG} from "../config/api.config";
 import {Observable} from "rxjs";
 import {Tecnico} from "../models/tecnico";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TecnicoService {
 
-  constructor(private http:HttpClient) { }
+    constructor(private http: HttpClient) {
+    }
 
-  findAll():Observable<Tecnico[]>{
-    return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
-  }
+    findAll(): Observable<Tecnico[]> {
+        return this.http.get<Tecnico[]>(`${API_CONFIG.baseUrl}/tecnicos`);
+    }
+
+    create(tecnico: Tecnico): Observable<Tecnico> {
+        return this.http.post<Tecnico>(`${API_CONFIG.baseUrl}/tecnicos`, tecnico);
+    }
 }
