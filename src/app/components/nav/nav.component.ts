a80b6813e0e51bb1dec22c3b6eafb9e7d2f6bb23
pedro <pedroeugeniocavalcanti@gmail.com>
2022-08-13 11:36:37 -0300

    Rota para componente CHAMADO-UPDATE criada

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -12,7 +12,7 @@ import {timeout} from "rxjs/operators";
 export class NavComponent implements OnInit {
 
   constructor(private router:Router, private authService:AuthService, private toast:ToastrService) {
-    this.router.navigate(["chamados/create"])
+    this.router.navigate(["chamados/update/1"])
   }
 
   ngOnInit(): void {
